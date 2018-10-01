27fbf330ba5a7f51c6f38f898bcbb3260e9cf5df
Ramana <ramana.testingforever@gmail.com>
2018-10-01 23:13:30 +0530

    Habba

---
+++ src/test/java/FirstPackage/FirstClass.java
@@ -0,0 +1,40 @@
+package FirstPackage;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstClass 
+{
+	WebDriver driver;
+  @Test
+  public void Add() 
+  {
+	  
+	  System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\Drivers\\chromedriver.exe");
+      driver = new ChromeDriver();	
+      
+      driver.manage().window().maximize();
+      
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      
+      driver.get("https://beckermediaqa.herokuapp.com/login");
+      
+      System.out.println(driver.getTitle());
+      
+      System.out.println(driver.getCurrentUrl());
+      
+      driver.navigate().to("http://beckermedia.slack.com/");
+      
+      System.out.println(driver.getTitle());
+      
+      System.out.println(driver.getCurrentUrl());
+      
+      
+      driver.quit(); //id,name, classname,tagname, xpath, css, linkte, part
+
+	  
+  }
+}
